5363d8b3c45c6dee189f480c9dc6c10f14dfc92c
Tessa Elyse <tessa.elise.robinson@gmail.com>
2022-11-23 12:28:04 -0700

    Fleshed out most of html

--- index.js
+++ index.js
@@ -114,17 +114,28 @@ function addMore () {
         fs.writeFileSync(
           "team.html",
           `
-                <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <div>${team[i].name}</div>
-    </head>
-    <body>
-                `
+          <!DOCTYPE html>
+          <html lang="en">
+            <head>
+              <meta charset="UTF-8" />
+              <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <link
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+                rel="stylesheet"
+                integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+                crossorigin="anonymous"
+              />
+              <link rel = "stylesheet" href="./assets/style.css" />
+              <link href="https://fonts.googleapis.com/css2?family=Short+Stack&display=swap" rel="stylesheet">
+              <title>♡ My Team ♡</title>
+            </head>
+            <body>
+            <header>
+              <h1 class="text-center">♡ My Team ♡</h1>
+            </header>
+            <main class = "container">  
+            <div class="row">`
         );
         for (let i = 0; i < team.length; i++) {
          fs.appendFileSync("team.html", `
